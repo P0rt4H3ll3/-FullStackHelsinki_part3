f177f7aa65a960239ccb07cc9ccd800a9f60f570
Phillip Radon <phillip.radon@yahoo.com>
2024-06-27 11:39:11 +0200

    3.15 Phonebook Datanbase step 3

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const morgan = require("morgan"); //middleware
 const cors = require("cors"); // with cors no cross origin error
+require("dotenv").config();
 const app = express();
+const Person = require("./model/person");
 
-//express.json(): Parses incoming requests with JSON payloads. This must be used before the request logger to ensure req.body is populated.
-app.use(cors());
-app.use(express.static("dist")); // to make express show static content (dist directory from frontend npm run build)
-app.use(express.json());
+morgan.token("postData", function getPostData(req) {
+  return JSON.stringify(req.body);
+});
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (
+    error.name === "ValidationError" ||
+    error.number === "ValidationError"
+  ) {
+    return response.status(400).json({
+      error: error.message,
+    });
+  }
+
+  next(error);
+};
 
 // Request logger middleware
 
@@ -14,10 +36,9 @@ app.use(express.json());
 
 //
 //costomizing the morgan logging middleware using morgan token to define that we want die data send in post requests (beware of legal requirments in Real Apps.)
-morgan.token("postData", function getPostData(req) {
-  return JSON.stringify(req.body);
-});
-
+app.use(express.static("dist")); // to make express show static content (dist directory from frontend npm run build)
+app.use(express.json());
+app.use(cors());
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :postData"
@@ -26,111 +47,74 @@ app.use(
 
 // Functions
 
-const generateID = () => {
-  const maxId =
-    persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
-  return maxId + 1;
-};
-
-const isDuplicate = (newPerson) =>
-  !!persons.find((person) => person.name === newPerson.name);
+// const isDuplicate = (newPerson) =>
+// / ! ! persons.find((person) => person.name === newPerson.name);
 //converting the .find result into a boolean using !!
 //same as if using Boolean(persons.find((person) => person.name === newPerson.name))
-
-// Hardcoded phone book
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 // defining routes
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
-  response.send(`
-    <p>
-        Phonebook has info for ${persons.length} people 
-        <br/> ${new Date()}
-    <p>    
-    `);
+  Person.find({}).then((person) => {
+    response.send(`
+      <p>
+          Phonebook has info for ${person.length} people 
+          <br/> ${new Date().toString()}
+      <p>    
+      `);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
   //params shows what the :id has
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  console.log(
-    "this is inside index.js app.delete this are the request.params.id : ",
-    request.params.id
-  );
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
-    // has to be number not phone
     return response.status(400).json({
       error: "name and phone missing",
     });
   }
 
-  const newPerson = {
+  const newPerson = new Person({
     name: body.name,
     number: body.number,
-    id: generateID(),
-  };
-  if (isDuplicate(newPerson)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  } else {
-    persons = persons.concat(newPerson);
-    response.json(newPerson);
-  }
+  });
+  newPerson.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+
+  // if (isDuplicate(newPerson)) {
+  //   return response.status(400).json({
+  //     error: "name must be unique",
+  //   });
 });
 
 // Middleware for handling unknown endpoints
 
 //Error-handling Middleware: Middleware for handling errors should be defined after your routes.
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
 app.use(unknownEndpoint);
-
+app.use(errorHandler);
 //start the server
 //we want to depoly on the internet so this has to change \
 /*
@@ -139,7 +123,7 @@ app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
 */
 // change to :
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
